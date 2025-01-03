d98aede90625f50877ce1494598130e74c26493b
devyej8753 <devyej8753@gmail.com>
2025-01-03 09:48:24 +0900

    [lyj] Select

--- lec01_basic/src/com/gn/practice/Select_One_Scanner.java
+++ lec01_basic/src/com/gn/practice/Select_One_Scanner.java
@@ -1,47 +1,51 @@
-package com.gn.study.practice;
+package com.gn.practice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import com.gn.study.model.vo.Test;
 
-public class Select_List_Vo {
+public class Select_One_Scanner {
+	
 	public static void main(String[] args) {
 		
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("조회하고자 하는 행의 번호를 입력하세요.");
+		int num = sc.nextInt();
+		
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			conn = DriverManager.getConnection(
-					"jdbc:mariadb://127.0.0.1:3306/jdbc_basic"
-					,"scott","tiger");
+			String url = "jdbc:mariadb://127.0.0.1:3306/jdbc_basic";
+			String id = "scott";
+			String pw = "tiger";
+			conn = DriverManager.getConnection(url, id, pw);
 			stmt = conn.createStatement();
-			rs = stmt.executeQuery("SELECT * FROM test");
-			List<Test> list = new ArrayList<Test>();
-			while(rs.next()) {
-				Test t = new Test();
+			String sql = "SELECT t_no , t_name ,t_date"
+					+ " FROM test"
+					+ " WHERE t_no ="+num;
+			rs = stmt.executeQuery(sql);
+			Test t = new Test();
+			if(rs.next()) {
 				t.setTestNo(rs.getInt("t_no"));
 				t.setTestName(rs.getString("t_name"));
 				t.setTestDate(rs.getTimestamp("t_date").toLocalDateTime());
-				list.add(t);
+
 			}
-			if(list.isEmpty()) {
-				System.out.println("조회된 결과가 없습니다.");
-			}else {
-				for(Test t : list) {
-					System.out.println(t);
-				}
-			}
+			System.out.println(t);
 			
-		}catch(Exception e) {
+		}catch(Exception e){
 			e.printStackTrace();
-			
 		}finally {
 			try {
 				rs.close();
@@ -51,7 +55,5 @@ public class Select_List_Vo {
 				e.printStackTrace();
 			}
 		}
-		
-		
 	}
 }
